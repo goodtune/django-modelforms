3346d353ac21ea7bc023a36baebe52e247b22237
Gary Reynolds <gary@touch.asn.au>
2019-01-11 22:08:51 +1100

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-modelforms',
-    version='0.2.1',
+    version='0.3.0',
     author='Touch Technology Pty Ltd',
     author_email='info@touchtechnology.com.au',
     maintainer='Gary Reynolds',
